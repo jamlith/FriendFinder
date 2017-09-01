c3881953e28d94a394db1032ca317838564543ed
James Litherland <jamlith@gmail.com>
2017-09-01 06:31:18 -0400

    and so it begins, already late.

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+ *  (FriendFinder):
+ *      server.js - The entry point for this marvel of modern technology
+ * 
+ */
+
+ var path = require('path');
+ var express = require('express');
+ var hndlbrs = require('express-handlebars')
